6d7bd6bf0bdd23953da3217fec675f5980cf30a3
Jordi <jordivilaxicola@gmail.com>
2022-04-24 11:24:43 +0200

    Initial commit

---
+++ src/main/java/com/jordivx/twitchangularjsspringboot/TwitchAngularjsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jordivx.twitchangularjsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchAngularjsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchAngularjsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordivx/twitchangularjsspringboot/TwitchAngularjsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordivx.twitchangularjsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchAngularjsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
